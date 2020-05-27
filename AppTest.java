c7446388bd2a66350abdaa2432ff916927b8edc2
Prathyushatirunahari <34310961+Prathyushatirunahari@users.noreply.github.com>
2020-05-27 23:15:22 +1200

    Add files via upload

---
+++ AppTest.java
@@ -0,0 +1,33 @@
+import org.junit.Test;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class AppTest {
+	@Test
+	
+	public void ploan() {
+		
+		System.out.println("good");
+
+
+}
+@BeforeTest
+
+public void prerequiste() {
+	
+	System.out.println("goodwer");
+	
+	
+}
+
+
+
+@AfterTest
+
+public void prerequiste1() {
+	
+	System.out.println("5646");
+	
+	
+}
+}
